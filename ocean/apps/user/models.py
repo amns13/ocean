f1eedbed5b60601f56532cd91b675072137433bb
amns13 <aman13b8@gmail.com>
2026-02-21 08:17:06 +0530

    feat: start user app

---
+++ ocean/apps/user/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    email = models.EmailField(_("email address"), blank=False, unique=True)
