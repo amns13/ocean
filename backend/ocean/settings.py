a5633b08703407cea4699bbff8ef62424dfae4f3
amns13 <aman13b8@gmail.com>
2026-02-21 13:04:47 +0530

    feat: jwt

--- backend/ocean/settings.py
+++ backend/ocean/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third party apps
+    "rest_framework",
+
     # My apps
     "ocean.apps.user",
     "ocean.apps.page"
@@ -124,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ],
+}
