0843f18c9a6fe8b7dd2b71f01ce295e0106ed14f
amns13 <aman13b8@gmail.com>
2026-04-11 17:26:52 +0530

    be-feat: session auth

---
+++ backend/ocean/apps/user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from ocean.apps.user.views import LoginView, LogOutView
+
+urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogOutView.as_view(), name="logout"),
+]
