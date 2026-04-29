af631b1f245ed2e6a2cd7079afac46690a1e2fab
amns13 <aman13b8@gmail.com>
2026-04-29 20:57:05 +0530

    fe-feat: support ctrl-s for save

--- frontend/src/views/PageView.vue
+++ frontend/src/views/PageView.vue
@@ -60,6 +60,10 @@ async function onBlockBlur(index) {
     blocks.value[index] = response.data;
   }
 }
+
+const handleSave = (index, value, html) => {
+  onBlockBlur(index);
+};
 </script>
 
 <template>
@@ -85,6 +89,7 @@ async function onBlockBlur(index) {
         v-model="block.content"
         class="block"
         @blur="onBlockBlur(index)"
+        @onSave="(value, html) => handleSave(index, value, html)"
       />
     </div>
   </div>
