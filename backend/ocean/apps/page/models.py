9dd6072d18cd787a046af832b69803feef0ecb9f
amns13 <aman13b8@gmail.com>
2026-05-24 10:35:15 +0530

    be-feat: added image models

--- backend/ocean/apps/page/models.py
+++ backend/ocean/apps/page/models.py
@@ -2,7 +2,12 @@ from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.text import slugify
 
-from ocean.apps.common.models import UidAndTimestampFieldModelMixin
+from ocean.apps.common.models import (
+    CreateUpdateTimeStampModelMixin,
+    SoftDeleteModelMixin,
+    UidAndTimestampFieldModelMixin,
+    UidFieldModelMixin,
+)
 
 User = get_user_model()
 
@@ -40,3 +45,27 @@ class Block(UidAndTimestampFieldModelMixin):
 
     def __str__(self) -> str:
         return f"{self.page_id}: Block {self.index}"
+
+
+def _page_image_upload_path(instance: "Image", filename: str) -> str:
+    return f"images/{instance.uploaded_by_id}/{instance.uid}/{filename}"
+
+
+class Image(SoftDeleteModelMixin, UidFieldModelMixin):
+    image = models.ImageField(upload_to=_page_image_upload_path)
+    content_hash = models.CharField(max_length=64, unique=True)
+    uploaded_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="uploaded_page_images")
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self) -> str:
+        return self.image.name
+
+
+class PageImage(SoftDeleteModelMixin, CreateUpdateTimeStampModelMixin):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE, related_name="instances")
+    page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name="images")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="images")
+    name = models.CharField(max_length=255, blank=True)
+
+    class Meta:
+        constraints = [models.UniqueConstraint(name="unique_image_within_page", fields=["page", "image"])]
